13ededf87e2ed419522d30cf8223a3040a09407c
Rocheandley Kwidama <rocheandley@gmail.com>
2021-11-15 08:02:37 +0100

    picture update

--- commands/ban.js
+++ commands/ban.js
@@ -21,8 +21,9 @@ module.exports = {
     }
 
     const userinfo = client.users.cache.get(member);
-    client.channels.cache.get("579799154449186868").send("https://media.giphy.com/media/26tP7Lltx6BaMhKfK/giphy.gif")
-
+  // client.channels.cache.get("579799154449186868").send("https://media.giphy.com/media/26tP7Lltx6BaMhKfK/giphy.gif")
+    
+    client.channels.cache.get("579799154449186868").send("https://i.pinimg.com/originals/f1/ea/82/f1ea82decb44a8fb291fecbd6bba04f1.png")
   //   let ballembed = new Discord.MessageEmbed()
   // .attachFiles("https://media.giphy.com/media/JrkbVRQA5adwle1ykt/giphy.gif")
   //         message.channel.send(ballembed);
