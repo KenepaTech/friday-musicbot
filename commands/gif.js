eeb669d8809866b72efeda68df7a660007570bf8
Rocheandley Kwidama <rocheandley@gmail.com>
2022-02-07 09:21:18 +0100

    Update gif.js

--- commands/gif.js
+++ commands/gif.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'Send gif',
     
     execute(interaction, client) {
-      client.client.channels.cache.get("579799154449186868").send("https://i.pinimg.com/originals/fe/2c/0f/fe2c0fda696f358d651a7a120367fbf8.jpg")
+      client.client.channels.cache.get("579799154449186868").send("https://pbs.twimg.com/media/CAx9L8TWcAA6zlU.png")
      
     },
   };
