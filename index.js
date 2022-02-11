8f0eb8fbb1de5fdf38fb5d1f936b9117b824d164
Rocheandley Kwidama <rocheandley@gmail.com>
2022-02-11 14:01:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -75,8 +75,8 @@ client.once("ready", () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
 //      channel.send('It\'s friday bitches');
-     channel.send('Its the fucking weekend hallelujah💃');
-     client.channels.cache.get("579799154449186868").send("https://media3.giphy.com/media/hzf2fgPynf7IIOsvF4/giphy.gif?cid=ecf05e47vqipig5skpi7vt5756gyr919w8k33xe9hydpynvy&rid=giphy.gif&ct=g")
+   //  channel.send('Its the fucking weekend hallelujah💃');
+     client.channels.cache.get("579799154449186868").send("https://i.giphy.com/media/3NUEWRGjmbslDQU3J0/giphy.webp")
     });
         
     // When you want to start it, use:
