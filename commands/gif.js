4d82e2731cda06d4f8dfbee1b3829fa2a47c1fea
Rocheandley Kwidama <rocheandley@gmail.com>
2022-03-01 08:18:53 +0100

    Update gif.js

--- commands/gif.js
+++ commands/gif.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'Send gif',
     
     execute(interaction, client) {
-      client.client.channels.cache.get("579799154449186868").send("https://pbs.twimg.com/media/CAx9L8TWcAA6zlU.png")
+      client.client.channels.cache.get("579799154449186868").send("https://www.azrivierenland.be/sites/default/files/inline-images/A6%20kaart_complimentendag_RECTO_meloen.jpg")
      
     },
   };
