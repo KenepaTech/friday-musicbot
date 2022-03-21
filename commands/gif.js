75ff28b7e91c196c6ae402a0cfd244b821a31b29
Rocheandley Kwidama <rocheandley@gmail.com>
2022-03-21 08:21:53 +0100

    monday

--- commands/gif.js
+++ commands/gif.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'Send gif',
     
     execute(interaction, client) {
-      client.client.channels.cache.get("579799154449186868").send("Is stealing other people's job also 'working at PwC'? ")
+      client.client.channels.cache.get("579799154449186868").send("https://scontent-amt2-1.xx.fbcdn.net/v/t1.6435-9/139920668_3854049614657605_3611615057553333309_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=730e14&_nc_ohc=3mKIN8ogT8oAX9OjVA-&_nc_ht=scontent-amt2-1.xx&oh=00_AT849HUStCGuzsUyZumt8yAfk16QXhUUUUSppqFXqLWRfQ&oe=625D1818")
      
     },
   };
