9bd3b72129c5433477009d113addc7d9abc144ff
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2022-08-11 15:28:09 +0200

    Update index.js
    SP 11/8: emoji toegevoegd

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ client.once("ready", () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
 //      channel.send('Grab the Wines!!');
-     channel.send("Grab a cold one, it's the weekend!!");
+     channel.send("Grab a cold one, it's the weekend!!🥵");
      client.channels.cache.get("579799154449186868").send("https://media.giphy.com/media/KflrAWaTSmIoA45yK2/giphy.gif")
     });
         
