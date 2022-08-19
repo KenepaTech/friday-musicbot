17b1d7e5ac44c20af87462acd33cf453d4e0272c
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2022-08-19 10:04:51 +0200

    Update index.js
    cheers ya plebs

--- index.js
+++ index.js
@@ -75,8 +75,8 @@ client.once("ready", () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
 //      channel.send('Grab the Wines!!');
-     channel.send("Grab a cold one, it's the weekend!!🥵");
-     client.channels.cache.get("579799154449186868").send("https://media.giphy.com/media/KflrAWaTSmIoA45yK2/giphy.gif")
+     channel.send("Cheers to the weekend!🍻🌴");
+     client.channels.cache.get("579799154449186868").send("https://media.giphy.com/media/7QgVKlWfxd7DqvsEra/giphy.gif")
     });
         
     // When you want to start it, use:
