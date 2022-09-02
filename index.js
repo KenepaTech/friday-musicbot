f4e4f9adb865a056254e1f6bf55a20647581595d
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2022-09-02 09:59:55 +0200

    How dare you
    This is mine

--- index.js
+++ index.js
@@ -75,8 +75,8 @@ client.once("ready", () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
 //      channel.send('Grab the Wines!!');
-     channel.send("🍻🍻☀️");
-     client.channels.cache.get("579799154449186868").send("https://c.tenor.com/0kUJ1ONV1MoAAAAC/mochi.gif")
+     channel.send("🍻Party mode activated🍻☀️");
+     client.channels.cache.get("579799154449186868").send("https://media.giphy.com/media/l2JhBoNin9yhqSDLO/giphy.gif")
     });
         
     // When you want to start it, use:
