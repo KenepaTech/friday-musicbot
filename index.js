ffb5c1b2ad5679f138830ae3f720c7af4011a07c
Rocheandley Kwidama <rocheandley@gmail.com>
2022-09-02 09:21:02 +0200

    prego

--- index.js
+++ index.js
@@ -75,8 +75,8 @@ client.once("ready", () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
 //      channel.send('Grab the Wines!!');
-     channel.send("Weekend time!🌴");
-     client.channels.cache.get("579799154449186868").send("https://media.giphy.com/media/3ohhwpzf1EmlkTUsO4/giphy.gif")
+     channel.send("🍻🍻☀️");
+     client.channels.cache.get("579799154449186868").send("https://c.tenor.com/0kUJ1ONV1MoAAAAC/mochi.gif")
     });
         
     // When you want to start it, use:
