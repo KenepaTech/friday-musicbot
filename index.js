50806ad23a05c8de9624263e1528677797bc2999
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2022-09-09 14:33:48 +0200

    Update index.js
    RIP HRH

--- index.js
+++ index.js
@@ -75,8 +75,8 @@ client.once("ready", () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
 //      channel.send('Grab the Wines!!');
-     channel.send("🍻Party mode activated🍻☀️");
-     client.channels.cache.get("579799154449186868").send("https://media.giphy.com/media/l2JhBoNin9yhqSDLO/giphy.gif")
+     channel.send("RIP Lizzy👑 Have a safe weekend y'all!");
+     client.channels.cache.get("579799154449186868").send("https://media.giphy.com/media/SS7BbrR0YJ3R6/giphy.gif")
     });
         
     // When you want to start it, use:
