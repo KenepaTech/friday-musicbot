08a9e03a225c6071636b8f2c60432a76ff75be12
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2023-02-02 19:10:06 +0100

    What do you call a beehive without an exit?
    Un-bee-lievable.

--- index.js
+++ index.js
@@ -76,8 +76,8 @@ client.once("ready", () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
 //      channel.send('Grab the Wines!!');
-     channel.send("Cheers to a relaxing weekend!💆‍♀️🥂");
-     client.channels.cache.get("579799154449186868").send("https://media.giphy.com/media/jTOlzdqrsVlZlxWqP5/giphy.gif")
+     channel.send("Enjoy the weekend!!!🥂");
+     client.channels.cache.get("579799154449186868").send("https://media.giphy.com/media/lAK7ACXBaEy4/giphy-downsized-large.gif")
     });
         
     // When you want to start it, use:
