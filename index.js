8a7d12c47c9deac1322ce1340367f4cf664826f4
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2023-02-10 16:22:17 +0100

    Why did the bullet end up losing his job?
    He got fired.

--- index.js
+++ index.js
@@ -76,8 +76,8 @@ client.once("ready", () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
 //      channel.send('Grab the Wines!!');
-     channel.send("Enjoy the weekend!!!🥂");
-     client.channels.cache.get("579799154449186868").send("https://media.giphy.com/media/lAK7ACXBaEy4/giphy-downsized-large.gif")
+     channel.send("Enjoy the weekend!!!😎☀️");
+     client.channels.cache.get("579799154449186868").send("https://media.giphy.com/media/jOyqJX2xsdpFj6sM9L/giphy.gif")
     });
         
     // When you want to start it, use:
