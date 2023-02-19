4eddcc364530cf2a85032baed6d5868399722228
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2023-02-20 00:15:19 +0100

    What do you call a woman that's really good at darts?
    Amy

--- index.js
+++ index.js
@@ -73,8 +73,8 @@ client.once("ready", () => {
      let scheduledMessage = new cron.CronJob('00 17 * * 5 ', () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
-     channel.send("Cheers to the weekend!!!😎☀️");
-     channel.send("https://media.giphy.com/media/ZzUz0TfQIZZ58K25co/giphy.gif")},
+     channel.send("Have a fantastic weekend!😎👾");
+     channel.send("https://media.giphy.com/media/SfnAG98m6OEzUOriA7/giphy.gif")},
           null,true,
           "Europe/Amsterdam");
     // When you want to start it, use:
