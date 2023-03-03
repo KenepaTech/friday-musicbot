002b28531fde16e12c4eab48f94c7de847a06962
Rocheandley Kwidama <rocheandley@gmail.com>
2023-03-03 22:00:45 +0100

    Update Client.js

--- client/Client.js
+++ client/Client.js
@@ -1,9 +1,24 @@
-const {Client, Collection, Intents} = require('discord.js');
+// const {Client, Collection, Intents} = require('discord.js');
+
+// module.exports = class extends Client {
+//   constructor(config) {
+//     super({
+//       intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS],
+//     });
+
+//     this.commands = new Collection();
+
+//     this.config = config;
+//   }
+// };
+
+
+const {Client, Collection, GatewayIntentBits, Partials} = require('discord.js');
 
 module.exports = class extends Client {
   constructor(config) {
     super({
-      intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS],
+      intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
     });
 
     this.commands = new Collection();
