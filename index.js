30f43d9961b90d5755162ee3fdbec1a174e0c04f
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2023-05-05 14:29:45 +0200

    What breed of dog can jump higher than buildings?
    Any dog, because buildings can’t jump.

--- index.js
+++ index.js
@@ -94,8 +94,8 @@ client.once("ready", () => {
      let scheduledMessage = new cron.CronJob('00 17 * * 5 ', () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
-     channel.send("Have a great one!🍻🍹");
-     channel.send("https://media.giphy.com/media/63Ggdsz2yn4mhhG5Cq/giphy.gif")},
+     channel.send("Cheers to the weekend!🍻");
+     channel.send("https://media.giphy.com/media/cqJ36SyeyNMFwWWasY/giphy.gif")},
           null,true,
           "Europe/Amsterdam");
     // When you want to start it, use:
