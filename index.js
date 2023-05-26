d1fc4f47ccdb8a68c5c49574c9e35bb0eeec3126
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2023-05-26 13:35:03 +0200

    How do you catch a bra?
    With a booby trap.

--- index.js
+++ index.js
@@ -94,8 +94,8 @@ client.once("ready", () => {
      let scheduledMessage = new cron.CronJob('00 17 * * 5 ', () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
-     channel.send("☀️Suns out, buns out!!!☀️ Have a nice weekend!😎🍻");
-     channel.send("https://media.giphy.com/media/3oEduZ8iYwB5GHIFDq/giphy.gif")},
+     channel.send("☀️Enjoy the long weekend!😎🍹");
+     channel.send("https://media.giphy.com/media/qyrJ4EKF8leIhTYIzx/giphy.gif")},
           null,true,
           "Europe/Amsterdam");
     // When you want to start it, use:
