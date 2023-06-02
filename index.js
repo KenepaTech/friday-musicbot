4491b42a3fcb1d3af60ddac32c8ba2c15dabb345
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2023-06-02 14:27:58 +0200

    When I was a kid, my mother told me I could be anyone I wanted to be.
    Turns out, identity theft is a crime.

--- index.js
+++ index.js
@@ -94,8 +94,8 @@ client.once("ready", () => {
      let scheduledMessage = new cron.CronJob('00 17 * * 5 ', () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
-     channel.send("☀️Enjoy the long weekend!😎🍹");
-     channel.send("https://media.giphy.com/media/qyrJ4EKF8leIhTYIzx/giphy.gif")},
+     channel.send("☀️Enjoy y'alls weekend!😎🍹");
+     channel.send("https://media.giphy.com/media/8sy5RV1jYKcnRDR8bd/giphy.gif")},
           null,true,
           "Europe/Amsterdam");
     // When you want to start it, use:
