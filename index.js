b5493c070b5642f4112d820fc6d25e9a12d2d31c
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2023-08-25 15:15:56 +0200

    What do sprinters eat before a race?
    Nothing, they fast.

--- index.js
+++ index.js
@@ -94,8 +94,8 @@ client.once("ready", () => {
      let scheduledMessage = new cron.CronJob('00 17 * * 5 ', () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
-     channel.send("Bon weer & vibes. Have a good one!🍻😎");
-     channel.send("https://media.giphy.com/media/mQSDrYE9H6LeMtJhdk/giphy.gif")},
+     channel.send("Have a good one!🌴🥥😎");
+     channel.send("https://media.giphy.com/media/7vQZanyufdRe0/giphy.gif")},
           null,true,
           "Europe/Amsterdam");
     // When you want to start it, use:
