0c1dd82b4d307cbf2761658e0bc4c724fea6c6db
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2023-09-15 12:52:26 +0200

    What is the least spoken language in the world?
    Sign language.

--- index.js
+++ index.js
@@ -94,8 +94,8 @@ client.once("ready", () => {
      let scheduledMessage = new cron.CronJob('00 17 * * 5 ', () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
-     channel.send("Felis wikènt everyone!☀️🌴");
-     channel.send("https://media.giphy.com/media/jTOlzdqrsVlZlxWqP5/giphy.gif")},
+     channel.send("Enjoy y'alls weekend!☀️🌴");
+     channel.send("https://media.giphy.com/media/3ornkdtVzQfIRpwfug/giphy.gif")},
           null,true,
           "Europe/Amsterdam");
     // When you want to start it, use:
