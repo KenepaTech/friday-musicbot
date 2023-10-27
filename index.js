260904a175ef48d1afd55e52180aa70e5c9003ad
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2023-10-27 16:40:14 +0200

    Why did the vampire need mouthwash?
    Because he had bat breath.

--- index.js
+++ index.js
@@ -94,8 +94,8 @@ client.once("ready", () => {
      let scheduledMessage = new cron.CronJob('00 17 * * 5 ', () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
-     channel.send("Enjoy the weekend!!💖");
-     channel.send("https://media.giphy.com/media/1BWKyYQX2K55GwiUPc/giphy.gif")},
+     channel.send("Pasa un dushi weekend!!💖");
+     channel.send("https://media.giphy.com/media/AOgwrHx4o49v2ZisoR/giphy-downsized-large.gif")},
           null,true,
           "Europe/Amsterdam");
     // When you want to start it, use:
