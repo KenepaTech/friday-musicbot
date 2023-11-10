4d0c014c1c4cf82a12115c6f432e04971000c485
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2023-11-10 13:44:38 +0100

    Where do pirates get their hooks?
    Second hand stores.

--- index.js
+++ index.js
@@ -94,8 +94,8 @@ client.once("ready", () => {
      let scheduledMessage = new cron.CronJob('00 17 * * 5 ', () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
-     channel.send("Enjoy your weekends!!🐮🦄");
-     channel.send("https://media.giphy.com/media/0YTUQkrGrhfiXVvdL9/giphy.gif")},
+     channel.send("🐸Dushi Weekend!🌴");
+     channel.send("https://media.giphy.com/media/23lzAGaZOXkomLSR8c/giphy.gif")},
           null,true,
           "Europe/Amsterdam");
     // When you want to start it, use:
