0f515536f823d44d815386378fdb93e516289c6b
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2024-01-05 16:27:51 +0100

    I don't go to funerals that start before noon.
    I guess I'm just not a mourning person.

--- index.js
+++ index.js
@@ -94,8 +94,8 @@ client.once("ready", () => {
      let scheduledMessage = new cron.CronJob('00 17 * * 5 ', () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
-     channel.send("🎄Hope your weekend sleighs✨☃️");
-     channel.send("https://media.giphy.com/media/NCnRKub8bkwuxEtzwF/giphy.gif")},
+     channel.send("Pasa un dushi weekend!🫶");
+     channel.send("https://media.giphy.com/media/MlZ5VSklGcprCoGEwe/giphy.gif")},
           null,true,
           "Europe/Amsterdam");
     // When you want to start it, use:
