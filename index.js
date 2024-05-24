63cdc3c014379068f525b91a69ba3db788d2e044
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2024-05-24 12:51:58 +0200

    You hear about he cows that stopped producing milk?
    It was a total udder failure.

--- index.js
+++ index.js
@@ -88,8 +88,8 @@ client.on('ready', function () {
      let scheduledMessage = new cron.CronJob('00 17 * * 5 ', () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
-     channel.send("😎Enjoy the long weekend!!🦆😎");
-     channel.send("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExczd6ZGs0ODlrdGwzZmQ5ZTN0Nmgxc2lpbWN3YzQ1MTJ5azN6djA5dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2cjT1mWzAPiQsSCb7l/giphy.gif")},
+     channel.send("🪩Dushi weekend mi gente!🕺");
+     channel.send("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdmt0NWV4ZTh5ejNhamRheTU5YTJrdWJmcTNsYTJhYmlleWxlcnp6YiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/jOyqJX2xsdpFj6sM9L/giphy.gif")},
           null,true,
           "Europe/Amsterdam");
     // When you want to start it, use:
