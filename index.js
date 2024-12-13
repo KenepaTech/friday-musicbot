99e85718d14bf8a0ca70a91baa1991ed713d06d0
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2024-12-13 16:45:42 +0100

    My uncle named his dogs Rolex and Timex.
    They're his watch dogs.

--- index.js
+++ index.js
@@ -88,8 +88,8 @@ client.on('ready', function () {
      let scheduledMessage = new cron.CronJob('00 17 * * 5 ', () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
-     channel.send("✨Dushi Weekend!🎄");
-     channel.send("https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjNweHk5d2RiMHBjaDRyaHRrZzFubXQ1eW90eXVpMzg4Z3pyamJ5ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/PkdHyto2hEOzoSqFUZ/giphy.gif")},
+     channel.send("✨Fijn weekend jongens!💕");
+     channel.send("https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTBqbjhoYnd2b3drZDVudmx2NzAyMjZiYTl5bHc3aXFhY2Jkemk3NyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/lAK7ACXBaEy4/giphy.gif")},
           null,true,
           "Europe/Amsterdam");
     // When you want to start it, use:
