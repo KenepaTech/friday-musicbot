d037336f2679872c58b47cedaddc83b7bfe87af3
Samantha Pikero <33733036+Trixsam@users.noreply.github.com>
2025-09-26 16:04:32 +0200

    What is orange and prickly?
    Idk either but had no joke today ktnxbye

--- index.js
+++ index.js
@@ -88,8 +88,8 @@ client.on('ready', function () {
      let scheduledMessage = new cron.CronJob('00 17 * * 5 ', () => {
      const guild = client.guilds.cache.get('579799154449186865');
      const channel = guild.channels.cache.get('579799154449186868');
-     channel.send("🫠Dushi weekend!!!☀️");
-     channel.send("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaWwwbjdnbWFmaDEzZTA0OXFlMWl1Mzd4MGl1emM5NGJ3ZWQwcWg0MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/FQLKJKMUrGxU3iRRVb/giphy.gif")},
+     channel.send("Dushi weekend vibes!!!☀️");
+     channel.send("https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExNW56aHU1d2oya3V1NGJjd21scnF3anFlOGU2OGRyaDBiZmg0OWdmbyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l0IsHsQK0O7u2d1Cg/giphy.gif")},
           null,true,
           "Europe/Amsterdam");
     // When you want to start it, use:
